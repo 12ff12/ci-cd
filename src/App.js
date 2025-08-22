33a7d33cbf2ac6869680970dc1ce4e2e9613bf86
Abdou Hassay <hassayabdou55@gmail.com>
2025-08-22 17:37:34 +0100

    ff

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 
-import './App.css';
-import { useState } from 'react';
+
 function App() {
-  const [count, setCount] = useState(0);
   return (
     <div className="App">
       <h1>sonar approved</h1>
