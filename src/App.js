2422169582ad3038863bd925e3e9234975f9a3f2
Abdou Hassay <hassayabdou55@gmail.com>
2025-08-22 16:27:54 +0100

    ff

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 
 import './App.css';
-
+import { useState } from 'react';
 function App() {
+  const [count, setCount] = useState(0);
   return (
     <div className="App">
       <h1>will work</h1>
